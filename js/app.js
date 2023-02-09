e5fcf8895188550c1117e89ebe334df48e6a614e
facundo <facucostafotografia@gmail.com>
2023-02-09 10:59:58 +0100

    Primer commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    
+    loop: true,
+    autoplay: {
+      delay: 1000,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  
